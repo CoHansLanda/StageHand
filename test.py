0d4240ed0b5dfbfe0f1b20f2a97dff9d3499ee03
CoHansLanda <coder0160@gmail.com>
2021-05-19 21:40:28 +0530

    navigate to web page

--- test.py
+++ test.py
@@ -1,17 +1,3 @@
-import os
-import discord
-token=os.environ.get('DISCORD_API_KEY')
-client=discord.Client()
-
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello')
-
-client.run(token)
+import sandbox
+movieName="The Godfather: Part II"
+sandbox.getMovieID(movieName)
